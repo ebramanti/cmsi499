0f75938d6d8ae557a14ede3017d1ab91dbd6ad41
Edward Bramanti <edward@bramanti.org>
2014-10-11 23:43:18 -0700

    Huge update with custom options and refreshing

--- ServerStats/html/pebble-settings.js
+++ ServerStats/html/pebble-settings.js
@@ -1,27 +1,24 @@
-//Setup to allow easy adding more options later
-function saveOptions() {
-  var serverURL = document.getElementById("serverURL");
-  var options = {
-    "serverURL": serverURL.value
-  }
-  return options;
-};
-
-var cancelButton = document.getElementById("cancel-button");
-cancelButton.addEventListener("click",
-  function() {
+$("#cancel-button").click(function() {
     console.log("Cancel");
     var location = "pebblejs://close";
     document.location = location;
-  },
-false);
+});
 
-var submitButton = document.getElementById("save-button");
-submitButton.addEventListener("click",
-  function() {
+$("#clear-button").click(function() {
+    $("#serverURL").val("");
+})
+
+$("#save-button").click(function() {
+    var serverURL = $("#serverURL").val();
+    var options = {
+      "serverURL": serverURL
+    }
     console.log("Submit");
-    var options = saveOptions();
     var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(options));
     document.location = location;
-  },
-false);
+});
+
+$(document).ready(function() {
+  var priorSite = window.location.search.substring(6)
+  $("#serverURL").val(priorSite);
+});
--- ServerStats/src/js/app.js
+++ ServerStats/src/js/app.js
@@ -2,32 +2,42 @@ var UI = require('ui');
 var ajax = require('ajax');
 var Settings = require('settings');
 
-Settings.config({
-  url: "http://my.cs.lmu.edu/~tbramant/serverstats/ServerStats.html"
-})
-
 var App = {};
-App.webAddress = Settings.option("serverURL") || "google.com";
+App.serverURL = Settings.option("serverURL") || "www.google.com";
 App.data = {};
+
+Settings.config(
+  {
+    url: "http://my.cs.lmu.edu/~tbramant/serverstats/ServerStats.html?site=" + App.serverURL
+  },
+  function(e) {
+    console.log('opening configurable');
+  },
+  function(e) {
+    App.serverURL = e.options.serverURL;
+    console.log('closed configurable');
+    console.log("new serverURL: " + e.options.serverURL);
+  }
+);
 
 App.init = function() {
   App.menu = new UI.Menu({
     sections: [{
       items: [{
         title: 'Server Name',
-        subtitle: App.webAddress
-      }, {
-        title: 'IP Address',
-        subtitle: App.data.ip
+        subtitle: App.serverURL
       }, {
         title: 'Server Status',
         subtitle: App.data.serverStatus
       }, {
+        title: 'IP Address',
+        subtitle: App.data.ip || "Unknown"
+      }, {
         title: 'IP Location',
-        subtitle: App.data.ipLocation
+        subtitle: App.data.ipLocation || "Unknown"
       }, {
         title: 'Region',
-        subtitle: App.data.regionLocation
+        subtitle: App.data.regionLocation || "Unknown"
       }]
     }]
   });
@@ -36,21 +46,41 @@ App.init = function() {
     textAlign: "center"
   });
   App.menu.on('select', function() {
+    var priorStatus = App.data.serverStatus;
+    var priorServer = App.menu.items(0)[0].subtitle;
     App.loading.show();
     ajax(
       {
-        url: 'http://ip-api.com/json/' + App.webAddress,
+        url: 'http://ip-api.com/json/' + App.serverURL,
         type: 'json',
         method: 'get'
       },
-      function(success) {
-        console.log("Success on Select Callback");
-        App.data.serverStatus = 'Online';
-        App.menu.item(0, 2, {subtitle: App.data.serverStatus});
+      function(data) {
+        if (data.status == "fail") {
+          console.log("Fail on Select Callback");
+          App.data.serverStatus = 'Offline';
+        } else {
+          console.log("Success on Select Callback");
+          App.data.serverStatus = 'Online';
+        }
+        if (App.serverURL != priorServer) {
+          App.menu.item(0, 0, {subtitle: App.serverURL});
+          App.menu.item(0, 2, {subtitle: data.query});
+          App.menu.item(0, 3, {subtitle: data.city + ', ' + data.region});
+          App.menu.item(0, 4, {subtitle: data.lat + ', ' + data.lon});
+        }
+        if (App.data.serverStatus != priorStatus) {
+          console.log("Change in server state!");
+          App.menu.item(0, 1, {subtitle: App.data.serverStatus});
+        }
       },
       function(failure) {
+        console.log("Non-200 Fail on Select Callback");
         App.data.serverStatus = 'Offline';
-        App.menu.item(0, 2, {subtitle: App.data.serverStatus});
+        if (App.serverURL != priorServer) {
+          App.menu.item(0, 0, {subtitle: "ERROR"});
+        }
+        App.menu.item(0, 1, {subtitle: App.data.serverStatus});
       }
     );
     setTimeout(function() {
@@ -62,20 +92,25 @@ App.init = function() {
 
 ajax(
   {
-    url: 'http://ip-api.com/json/' + App.webAddress,
+    url: 'http://ip-api.com/json/' + App.serverURL,
     type: 'json',
     method: 'get',
   },
   function(data) {
-    App.data.ip = data.query;
-    App.data.regionLocation = data.city + ', ' + data.region;
-    App.data.ipLocation = data.lat + ', ' + data.lon;
-    App.data.serverStatus = 'Online';
     console.log(data.status.toUpperCase());
-    App.init();
+    if (data.status == "fail") {
+      App.data.serverStatus = 'Offline';
+      App.init();
+    } else {
+      App.data.ip = data.query;
+      App.data.regionLocation = data.city + ', ' + data.region;
+      App.data.ipLocation = data.lat + ', ' + data.lon;
+      App.data.serverStatus = 'Online';
+      App.init();
+    }
   },
   function(failure) {
-    App.data.serverStatus = 'Offline';
+    App.data.serverStatus = 'API_ERROR';
     App.init();
   }
 );
